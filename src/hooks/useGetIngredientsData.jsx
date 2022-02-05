91369ca46fd4c0e94e8b83b9377e15dddc47f1f4
Sandra <sandriniia_grebentcova@riseup.net>
2022-02-05 17:50:42 +0100

    create useGetIngredientsData hook

---
+++ src/hooks/useGetIngredientsData.jsx
@@ -0,0 +1,19 @@
+import {useState, useEffect} from 'react'
+
+const useGetIngredientsData = () => {
+
+  const [products, setProducts] = useState([])
+
+  const ingredients_api = 'https://norma.nomoreparties.space/api/ingredients';
+
+  useEffect(() => {
+    fetch(ingredients_api)
+      .then((res) => { return res.json() })
+      .then((data) => { setProducts(data) })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return products;
+}
+
+export default useGetIngredientsData;
