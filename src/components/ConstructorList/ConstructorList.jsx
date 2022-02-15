535ace2cc9ec3d53b929eb0f863d77750f77602e
Sandra <sandriniia_grebentcova@riseup.net>
2022-02-15 18:14:45 +0100

    change ConstructorList,add find method to find bun;delete buns from middle container

--- src/components/ConstructorList/ConstructorList.jsx
+++ src/components/ConstructorList/ConstructorList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import constructorListStyle from './constructorList.module.css';
 import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
 import ProductsContext from '../../context/ProductsContext';
@@ -7,39 +7,49 @@ const ConstructorList = () => {
   const ingredientsContext = useContext(ProductsContext);
   const products = ingredientsContext.products;
 
+  const bun = products.find((item) => {
+    return item.type === 'bun';
+  });
+
   return (
     <>
-      <div className={`${constructorListStyle.element_container} mb-4`}>
-        <ConstructorElement
-          type='top'
-          isLocked={true}
-          text='Краторная булка N-200i (верх)'
-          price={200}
-          thumbnail='https://code.s3.yandex.net/react/code/bun-02.png'
-        />
-      </div>
+      {bun && (
+        <div className={`${constructorListStyle.element_container} mb-4`}>
+          <ConstructorElement
+            type='top'
+            isLocked={true}
+            text={`${bun.name} (верх)`}
+            price={bun.price}
+            thumbnail={bun.image}
+          />
+        </div>
+      )}
       <div className={constructorListStyle.middle_container}>
         {products &&
           products.map((item, index) => {
             return (
-              <div
-                className={`${constructorListStyle.element_container} mb-4`}
-                key={`${item._id}_${index}`}
-              >
-                <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
-              </div>
+              item.type !== 'bun' && (
+                <div
+                  className={`${constructorListStyle.element_container} mb-4`}
+                  key={`${item._id}_${index}`}
+                >
+                  <ConstructorElement text={item.name} price={item.price} thumbnail={item.image} />
+                </div>
+              )
             );
           })}
       </div>
-      <div className={`${constructorListStyle.element_container} mb-4`}>
-        <ConstructorElement
-          type='bottom'
-          isLocked={true}
-          text='Краторная булка N-200i (низ)'
-          price={200}
-          thumbnail='https://code.s3.yandex.net/react/code/bun-02.png'
-        />
-      </div>
+      {bun && (
+        <div className={`${constructorListStyle.element_container} mb-4`}>
+          <ConstructorElement
+            type='bottom'
+            isLocked={true}
+            text={`${bun.name} (низ)`}
+            price={bun.price}
+            thumbnail={bun.image}
+          />
+        </div>
+      )}
     </>
   );
 };
