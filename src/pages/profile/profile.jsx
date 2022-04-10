656034986bb75154b849446564b78327adb8d705
Sandra <sandriniia_grebentcova@riseup.net>
2022-04-10 16:11:42 +0200

    add styles to profile page

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
 import profileStyles from './profile.module.css';
 import { getUserInfo, changeUserInfo } from '../../services/slices/userInfoSlice';
 import { refreshUserToken } from '../../services/slices/userInfoSlice';
 
 const Profile = () => {
   const dispatch = useDispatch();
+
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const userName = useSelector((state) => state.user.name);
   const userEmail = useSelector((state) => state.user.email);
@@ -24,6 +29,10 @@ const Profile = () => {
 
   const changePasswordHandler = (event) => {
     setPassword(event.target.value);
+  };
+
+  const onIconClick = (ref) => {
+    ref.current.focus();
   };
 
   const submitHandler = (event) => {
@@ -48,27 +57,47 @@ const Profile = () => {
   };
 
   return (
-    <section>
-      <h1>Профиль</h1>
-      <form onSubmit={submitHandler}>
-        <input id='name' name='name' type='text' value={name || ''} onChange={changeNameHandler} />
-        <button type='submit'></button>
-        <input
-          id='email'
-          name='email'
-          type='email'
-          value={email || ''}
+    <section className={profileStyles.profile}>
+      <form className={profileStyles.form} onSubmit={submitHandler}>
+        <Input
+          type={'text'}
+          placeholder={'Имя'}
+          onChange={changeNameHandler}
+          icon={'EditIcon'}
+          value={name || ''}
+          name={'name'}
+          error={false}
+          onIconClick={() => onIconClick(nameInputRef)}
+          errorText={'Ошибка'}
+          size={'default'}
+          ref={nameInputRef}
+        />
+        <Input
+          type={'email'}
+          placeholder={'Логин'}
           onChange={changeEmailHandler}
+          icon={'EditIcon'}
+          value={email || ''}
+          name={'email'}
+          error={false}
+          onIconClick={() => onIconClick(emailInputRef)}
+          errorText={'Ошибка'}
+          size={'default'}
+          ref={emailInputRef}
         />
-        <button type='submit'></button>
-        <input
-          id='password'
-          name='password'
-          type='password'
-          value={password || ''}
+        <Input
+          type={'password'}
+          placeholder={'Пароль'}
           onChange={changePasswordHandler}
+          icon={'EditIcon'}
+          value={password || ''}
+          name={'password'}
+          error={false}
+          onIconClick={() => onIconClick(passwordInputRef)}
+          errorText={'Ошибка'}
+          size={'default'}
+          ref={passwordInputRef}
         />
-        <button type='submit'></button>
       </form>
     </section>
   );
