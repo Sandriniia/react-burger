608f4356be6b257713500b45e166b3a905bfadfd
Sandra <sandriniia_grebentcova@riseup.net>
2022-05-18 22:02:47 +0200

    change ingredient, IngredientDetails to type script

--- src/pages/ingredient/ingredient.tsx
+++ src/pages/ingredient/ingredient.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import IngredientDetails from '../../components/IngredientDetails/IngredientDetails';
+import { useAppSelector } from '../../services/types/hooks';
 
-const Ingredient = () => {
-  const { id } = useParams();
+const Ingredient: FC = () => {
+  const { id } = useParams<{ id: string }>();
 
-  const products = useSelector((state) => state.products.products);
+  const products = useAppSelector((state) => state.products.products);
   const currentIngredient = products.find((product) => product._id === id);
 
   return (
